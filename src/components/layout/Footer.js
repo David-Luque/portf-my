27082e402db21b46c2dc31788b1e9b9e4fc9735a
David L <dvdlq99@gmail.com>
2021-10-11 17:45:51 +0200

    Uploaded sections draft

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2>Footer comp</h2>
+        </footer>
+    );
+}
+ 
+export default Footer;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Header = ()=>{
+    return (
+        <h2>Header</h2>
+    );
+};
+
+export default Header;
