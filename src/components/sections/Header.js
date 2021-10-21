377cb76caab164b31925dea7593466f040ef8a1a
David L <dvdlq99@gmail.com>
2021-10-21 17:41:55 +0200

    styled nav and header

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -6,13 +6,19 @@ const Header = () => {
         <header className="header">
             <div className="header__text-box">
                 <h1 className="heading-primary">
-                    <span className="heading-primary--main">David Luque</span>
+                    <span className="heading-primary--main">David&nbsp;Luque</span>
                     <span className="heading-primary--sub">Full stack web developer</span>
                 </h1>
                 <div className="header__line"></div>
             </div>
+            <p className="header__arrows">
+                <span>&#62;</span>
+                <span>&#62;</span>
+                <span>&#62;</span>
+                <span>&#62;</span>
+                <span>&#62;</span>
+            </p>
         </header>
-        
     );
 };
 
