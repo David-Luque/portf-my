b13810f6cf4848452f6f8929bcda1c6ef0f053cb
David L <dvdlq99@gmail.com>
2021-10-22 21:50:44 +0200

    improved footer styles

--- src/components/sections/Technologies.js
+++ src/components/sections/Technologies.js
@@ -9,43 +9,59 @@ const Technologies = () => {
             </h2>
             <ul className="section-technologies__content">
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Basics</h3>
-                    <p className="technologies__tech">HTML</p>
-                    <p className="technologies__tech">CSS / SASS / SCSS</p>
-                    <p className="technologies__tech">Javascript</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Basics
+                    </h3>
+                    <p className="section-technologies__tech">HTML</p>
+                    <p className="section-technologies__tech">CSS / SASS / SCSS</p>
+                    <p className="section-technologies__tech">Javascript</p>
                 </li>
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Frontend frameworks</h3>
-                    <p className="technologies__tech">React</p>
-                    <p className="technologies__tech">Next.js & Gatsby</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Frontend frameworks
+                    </h3>
+                    <p className="section-technologies__tech">React</p>
+                    <p className="section-technologies__tech">Next.js & Gatsby</p>
                 </li>
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Server frameworks</h3>
-                    <p className="technologies__tech">Node.js & Express.js</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Server frameworks
+                    </h3>
+                    <p className="section-technologies__tech">Node.js & Express.js</p>
                     
                 </li>
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Databases</h3>
-                    <p className="technologies__tech">MongoDB</p>
-                    <p className="technologies__tech">MySQL</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Databases
+                    </h3>
+                    <p className="section-technologies__tech">MongoDB</p>
+                    <p className="section-technologies__tech">MySQL</p>
                 </li>
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Testing</h3>
-                    <p className="technologies__tech">Jest, Cypress, React-testing-library</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Testing
+                    </h3>
+                    <p className="section-technologies__tech">Jest, Cypress, React-testing-library</p>
                 </li>
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Version control</h3>
-                    <p className="technologies__tech">Git & GitHub</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Version control
+                    </h3>
+                    <p className="section-technologies__tech">Git & GitHub</p>
                 </li>
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Content management systems</h3>
-                    <p className="technologies__tech">Firebase, DatoCMS, Strapi</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Content management systems
+                    </h3>
+                    <p className="section-technologies__tech">Firebase, DatoCMS, Strapi</p>
                 </li>
                 <li className="section-technologies__block">
-                    <h3 className="heading-tertiary">Other tools</h3>
-                    <p className="technologies__tech">Redux</p>
-                    <p className="technologies__tech">REST APIs & GraphQL</p>
-                    <p className="technologies__tech">Styled components</p>
+                    <h3 className="heading-tertiary heading-tertiary--light">
+                        Other tools
+                    </h3>
+                    <p className="section-technologies__tech">Redux</p>
+                    <p className="section-technologies__tech">REST APIs & GraphQL</p>
+                    <p className="section-technologies__tech">Styled components</p>
                 </li>
             </ul>
         </section>
