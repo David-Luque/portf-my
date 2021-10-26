09ad2bfc60b0da8e46e81e1046dc7909b78726f0
David L <dvdlq99@gmail.com>
2021-10-26 21:42:15 +0200

    added media queries to phone viewport

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
                     <span className="heading-primary--main">David&nbsp;Luque</span>
                     <span className="heading-primary--sub">Full stack web developer</span>
                 </h1>
-                <div className="header__line"></div>
             </div>
+            <div className="header__line"></div>
             {/* <p className="header__arrows">
                 <span>&#62;</span>
                 <span>&#62;</span>
