5db58b9301f36a24c5d3f31b7c9477d2d757cf93
David L <dvdlq99@gmail.com>
2021-11-02 18:30:53 +0100

    Revised and fixed styles for all viewport breakpoints

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -13,6 +13,9 @@ const Layout = ({ children })=>{
                 <meta name="description" content="David Luque personal portfolio"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <link rel="shortcut icon" type="image/png" href={iconImg}/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,700;1,400&display=swap" rel="stylesheet"/>
             </Helmet>
 
             <Nav />
